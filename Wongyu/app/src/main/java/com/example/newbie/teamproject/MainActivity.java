14b5bdfd1bf52df34509ce0258109a33958e97c9
WongyuLee <37143822+WongyuLee@users.noreply.github.com>
2018-12-20 18:00:26 +0900

    add activity

--- Wongyu/app/src/main/java/com/example/newbie/teamproject/MainActivity.java
+++ Wongyu/app/src/main/java/com/example/newbie/teamproject/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.newbie.teamproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickButton(View view) {
+        switch (view.getId()) {
+            case R.id.button1 :
+                Intent i = new Intent(this, DetectActivity.class);
+                startActivity(i);
+                break;
+            case R.id.button2 :
+                break;
+            case R.id.button3 :
+                break;
+        }
     }
 }
